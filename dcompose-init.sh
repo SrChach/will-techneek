1c6783a3adffad44bc7f4b8f1cfd70d6d480720d
Ignacio Martinez <gsiko@gmail.com>
2024-09-14 13:02:09 -0600

    CI: Added commands for installing deps

--- dcompose-init.sh
+++ dcompose-init.sh
@@ -1,4 +1,7 @@
 docker compose exec app composer install \
-	&& docker compose exec app php artisan l5-swagger:generate
+	&& docker compose exec app php artisan l5-swagger:generate \
 	&& docker compose exec app php artisan migrate
+
+docker compose exec app npm install --legacy-peer-deps \
+	&& docker compose exec app npm run build
 
